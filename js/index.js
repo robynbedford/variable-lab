d0e9268196567ff0ed10a08a518885c187c710dd
Robyn Bedford <robynbedford@Robyns-MacBook-Air.local>
2019-10-30 15:49:22 -0400

    variable-lab

---
+++ js/index.js
@@ -0,0 +1,67 @@
+// Exercise 1: The Fortune Teller
+console.log('Exercise One')
+
+let jobTitle = 'Web Developer';
+let geolocation = 'Toronto';
+let annualsal = '60,000';
+let companyName = 'Humber';
+console.log('You will be a ' + jobTitle + ' in ' + geolocation + ' making ' + annualsal + ' for ' + companyName); 
+
+// Exercise 2: The Age Calculator 
+console.log('Exercise Two')
+
+let currentYear  = 2019;
+let birthYear = 1992;
+let age  = currentYear - birthYear;
+console.log('They are ' + age + ' years old ');
+
+// Exercise 3: The Lifetime Supply Calculator 
+console.log('Exercise Three')
+
+let currentAge = "27";
+let maxAge = "85";
+let amount_daily = "5";
+
+var endamount=(amount_daily * 365) * (maxAge - currentAge); 
+console.log('You will need '+ endamount + ' to last you until the ripe old age of ' + maxAge)
+
+
+// Exercise 4: The Geometrizer
+console.log('Exercise 4')
+
+let circleRadius = "8";
+let circlePie = "3.14";
+let circumference= (2 * circlePie * circleRadius)
+
+console.log('The circumference is ' + circumference);
+
+let circleArea=((circleRadius ** 2) * circlePie);
+console.log('The area is ' + circleArea);
+
+
+// Exersie 5: The Temperacture Converter 
+console.log('Exercise 5')
+
+var tempcelcius = '30';
+var tempfahrenheit = ((30 * 9/5) + 32);
+
+console.log(tempcelcius + ' is ' + tempfahrenheit)
+
+var tempfahrenheit = "104";
+var tempcelcius = ((tempfahrenheit - 32) * 5/9);
+
+console.log(tempfahrenheit + ' is ' + tempcelcius);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
